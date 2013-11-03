ade32fb20f8b9e495a9dd0fca34f7a1a61f8c285
Nicolas Viennot <nicolas@viennot.biz>
2013-11-02 22:17:54 -0400

    Update recipes for latest versions

--- site-cookbooks/tmate/attributes/default.rb
+++ site-cookbooks/tmate/attributes/default.rb
@@ -2,3 +2,4 @@
  default[:tmate][:keys_dir] = "#{Dir.home}/tmate-slave/keys"
  default[:tmate][:log_path] = "/var/log/tmate"
  default[:tmate][:listen_port] = 22
+ default[:tmate][:domain] = "tmate.io"
--- site-cookbooks/tmate/recipes/default.rb
+++ site-cookbooks/tmate/recipes/default.rb
@@ -37,7 +37,8 @@ template '/etc/init/tmate.conf' do
   variables :app_path    => node[:tmate][:app_path],
             :log_path    => node[:tmate][:log_path],
             :keys_dir    => node[:tmate][:keys_dir],
-            :listen_port => node[:tmate][:listen_port]
+            :listen_port => node[:tmate][:listen_port],
+            :hostname    => "#{node[:hostname]}.#{node[:tmate][:domain]}"
 end
 
 service "tmate" do
